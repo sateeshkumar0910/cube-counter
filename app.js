90a50f5ae544444c4da0cd5c7791d58415bac972
C. Sateeshkumar <sateeshkumard41@gmail.com>
2022-08-16 21:52:41 +0530

    Simple cube counter

---
+++ app.js
@@ -0,0 +1,17 @@
+//console.log("hello sateesh");
+
+/*let email ='sateesh@gmail.com';
+console.log('email');*/
+
+/*function changeColor(){
+    document.getElementById("title").style.color = " yellow"
+}*/
+
+function cube()
+{
+    let num = document.getElementById('number').value;
+    let result = num * num * num;
+    document.getElementById('result').innerHTML = result;
+
+
+}
